5d564c16e4dbd1269a3dcf6ff7cbbda9d516ed57
Niko Matsakis <niko@alum.mit.edu>
2011-10-13 21:52:43 -0700

    improve prelude detection, tokenizer

--- src/assertion-analyzer.py
+++ src/assertion-analyzer.py
@@ -15,44 +15,32 @@ import sys
 # __________________________________________________________________
 # Very simple C tokenizer
 
-# Fixed tokens:
-KIND_START = 'START'
-KIND_EOF = 'EOF'
-KIND_LBRACE = '{'
-KIND_RBRACE = '}'
-KIND_COLON = ':'
-KIND_COLON_COLON = '::'
-KIND_LPAREN = '('
-KIND_RPAREN = ')'
-KIND_COMMA = ','
-KIND_SEMI = ';'
-
-# Other tokens:
-KIND_ID = 'ID'
-KIND_STRING = '""'
-KIND_OTHER = '?'
-
 class Token(object):
 	def __init__(self, kind, text):
 		self.kind = kind
 		self.text = text
 		
 	def is_id(self, text):
-		return self.kind == KIND_ID and self.text == text
+		return self.kind == 'ID' and self.text == text
 		
 	def __repr__(self):
 		return "Token(%r, %r)" % (self.kind, self.text)
 		
-TOK_START = Token(KIND_START, "")
-TOK_EOF = Token(KIND_EOF, "")
-TOK_LBRACE = Token(KIND_LBRACE, "{")
-TOK_RBRACE = Token(KIND_RBRACE, "}")
-TOK_LPAREN = Token(KIND_LPAREN, "(")
-TOK_RPAREN = Token(KIND_RPAREN, ")")
-TOK_COMMA = Token(KIND_COMMA, ",")
-TOK_COLON = Token(KIND_COLON, ":")
-TOK_COLON_COLON = Token(KIND_COLON_COLON, "::")
-TOK_SEMI = Token(KIND_SEMI, ";")
+TOK_START = Token('START', "")
+TOK_EOF = Token('EOF', "")
+TOK_LBRACE = Token('{', "{")
+TOK_RBRACE = Token('}', "}")
+TOK_LPAREN = Token('(', "(")
+TOK_RPAREN = Token(')', ")")
+TOK_LBRACKET = Token('[', '[')
+TOK_RBRACKET = Token(']', ']')
+TOK_LT = Token('<', "<")
+TOK_GT = Token('>', ">")
+TOK_COMMA = Token(',', ",")
+TOK_COLON = Token(':', ":")
+TOK_COLON_COLON = Token('::', "::")
+TOK_SEMI = Token(';', ";")
+TOK_EQ = Token('=', "=")
 
 class ParseError(object):
 	def __init__(self, text, offset):
@@ -84,6 +72,10 @@ class Tokenizer(object):
 				self.tok = TOK_LBRACE
 			elif equals('}'):
 				self.tok = TOK_RBRACE
+			elif equals('['):
+				self.tok = TOK_LBRACKET
+			elif equals(']'):
+				self.tok = TOK_RBRACKET
 			elif equals('::'):
 				self.tok = TOK_COLON_COLON
 			elif equals(':'):
@@ -94,20 +86,22 @@ class Tokenizer(object):
 				self.tok = TOK_COMMA
 			elif equals('"'):
 				end = self.find_string_end()
-				self.tok = Token(KIND_STRING, self.text[start:end])
+				self.tok = Token('STRING', self.text[start:end])
 			elif equals('/*'):
 				self.position = self.find_comment_end()
 			elif equals('//'):
 				self.position = self.find_next_line()
+			elif equals('='):
+				self.tok = TOK_EQ
 			elif self.text[self.position].isspace():
 				self.position += 1
 			else:
 				ch = self.text[self.position]
 				if ch.isalpha() or ch == '_':
 					end = self.find_identifier_end()
-					self.tok = Token(KIND_ID, self.text[start:end])
+					self.tok = Token('ID', self.text[start:end])
 				else:
-					self.tok = Token(KIND_OTHER, ch)
+					self.tok = Token('OTHER', ch)
 		
 		self.position += len(self.tok.text)
 		return prev_tok
@@ -159,12 +153,13 @@ class Tokenizer(object):
 				break
 		return position
 		
-	def assertion_text(self):
-		assert self.tok.kind == KIND_LPAREN
+	def assertion_contents(self):
+		assert self.tok == TOK_LPAREN
 		counter = 1
 		text = self.text
 		position = start_position = self.position
 		max_position = len(text)
+		contents = []
 		while position < max_position:
 			ch = text[position]
 			if ch == '(':
@@ -174,7 +169,11 @@ class Tokenizer(object):
 			elif ch == ')':
 				self.position = position + 1
 				self.tok = TOK_RPAREN
-				return self.text[start_position:position]
+				contents += [text[start_position:position].strip()]
+				return contents
+			elif ch == ',' and counter == 1:
+				contents += [text[start_position:position].strip()]
+				start_position = position + 1
 			position += 1
 		raise ParseError("Unclosed parenthesis", start_position)
 		
@@ -182,15 +181,16 @@ class Tokenizer(object):
 # Assertion Gatherer
 
 class Assertion(object):
-	def __init__(self, text, file_name, offset, stmts):
-		self.text = text
+	def __init__(self, contents, file_name, offset, prelude, stmts):
+		self.contents = contents
 		self.file_name = file_name
 		self.offset = offset
+		self.prelude = prelude
 		self.stmts = stmts
 		
 	def __repr__(self):
-		return "Assertion(%r, %r, %r, %r)" % (
-			self.text, self.file_name, self.offset, self.stmts)
+		return "Assertion(%r, %r, %r, %r, %r)" % (
+			self.contents, self.file_name, self.offset, self.prelude, self.stmts)
 
 # Start in state OUTER.
 #
@@ -238,6 +238,7 @@ class Gather(object):
 	def function_body(self):
 		nesting = 1
 		statements = 0
+		prelude = True
 		while self.tokenizer.tok != TOK_EOF:
 			tok = self.tokenizer.next_token()
 			if tok == TOK_RBRACE:
@@ -249,11 +250,15 @@ class Gather(object):
 				nesting += 1
 			elif tok == TOK_SEMI:
 				statements += 1
+			elif tok in [TOK_EQ, TOK_LPAREN] or tok.kind == 'OTHER':
+				prelude = False
 			elif tok.is_id("NS_ASSERTION") or tok.is_id("JS_ASSERT"):
 				if self.tokenizer.tok == TOK_LPAREN:
 					offset = self.tokenizer.position
-					text = self.tokenizer.assertion_text()
-					assertion = Assertion(text, self.file_name, offset, statements)
+					contents = self.tokenizer.assertion_contents()
+					assertion = Assertion(
+						contents, self.file_name, 
+						offset, prelude, statements)
 					self.assertions.append(assertion)
 					
 def main(args):
